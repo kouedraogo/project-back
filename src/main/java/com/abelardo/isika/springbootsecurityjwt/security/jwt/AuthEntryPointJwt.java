0a750d89755eccc068da0df2567e5cf802f3bc47
Kiswendsida Abel Ouedraogo <halloween452001@yahoo.fr>
2022-04-15 11:22:27 +0200

    updating files

--- src/main/java/com/abelardo/isika/springbootsecurityjwt/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/abelardo/isika/springbootsecurityjwt/security/jwt/AuthEntryPointJwt.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-/*
+
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
@@ -29,7 +29,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
 	}
 }
-*/
+/*
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
@@ -54,3 +54,4 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
   }
 
 }
+*/
