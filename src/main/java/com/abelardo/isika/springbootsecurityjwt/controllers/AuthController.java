8769179a3964fe525e98144c216e1d3d876d1e0c
Kiswendsida Abel Ouedraogo <halloween452001@yahoo.fr>
2022-05-05 16:57:29 +0200

    adding controllers for admin CRUD page

--- src/main/java/com/abelardo/isika/springbootsecurityjwt/controllers/AuthController.java
+++ src/main/java/com/abelardo/isika/springbootsecurityjwt/controllers/AuthController.java
@@ -1,13 +1,16 @@
 package com.abelardo.isika.springbootsecurityjwt.controllers;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,6 +18,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -128,4 +134,50 @@ public class AuthController {
 
 		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
 	}
+
+	@GetMapping("/users")
+  	public ResponseEntity <List<User>> getAllUsers() {
+	try {
+		List<User> users = new ArrayList<User>();
+		userRepository.findAll();
+		if (users.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	    }catch (Exception e) {
+		return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+	   }
+	}
+
+	@GetMapping("/users/{id}")
+  	public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+    Optional<User> userData = userRepository.findById(id);
+    if (userData.isPresent()) {
+      return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+    } 
+    else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+  	}
+	
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") long id) {
+	    try {
+	      userRepository.deleteById(id);
+	      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    } catch (Exception e) {
+	      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	  }
+
+	@DeleteMapping("/users")
+	  public ResponseEntity<HttpStatus> deleteAllUsers() {
+	    try {
+	      userRepository.deleteAll();
+	      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    } catch (Exception e) {
+	      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	  }
+
 }
