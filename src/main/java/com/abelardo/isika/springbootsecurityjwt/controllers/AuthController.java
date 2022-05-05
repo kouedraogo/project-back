5355b122a84b57a0fc08ad6ab254422bf56a7d64
Kiswendsida Abel Ouedraogo <halloween452001@yahoo.fr>
2022-05-05 17:50:55 +0200

    modifing getAllUsers controler

--- src/main/java/com/abelardo/isika/springbootsecurityjwt/controllers/AuthController.java
+++ src/main/java/com/abelardo/isika/springbootsecurityjwt/controllers/AuthController.java
@@ -135,19 +135,25 @@ public class AuthController {
 		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
 	}
 
-	@GetMapping("/users")
-  	public ResponseEntity <List<User>> getAllUsers() {
-	try {
-		List<User> users = new ArrayList<User>();
-		userRepository.findAll();
-		if (users.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<>(users, HttpStatus.OK);
-	    }catch (Exception e) {
-		return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-	   }
-	}
+//	@GetMapping("/users")
+//  	public ResponseEntity <List<User>> getAllUsers() {
+//	try {
+//		List<User> users = new ArrayList<User>();
+//		userRepository.findAll();
+//		if (users.isEmpty()) {
+//			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//		}
+//		return new ResponseEntity<>(users, HttpStatus.OK);
+//	    }catch (Exception e) {
+//		return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//	   }
+//	}
+	
+	@GetMapping(path = "/users")
+    public List<User> getAllUsers() {
+		List<User> user = userRepository.findAll();
+		return user;
+    }
 
 	@GetMapping("/users/{id}")
   	public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
