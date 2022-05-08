3649ab3ff6619c1c8435513676f3575e699897e7
Kiswendsida Abel Ouedraogo <halloween452001@yahoo.fr>
2022-05-08 09:03:42 +0200

    modifing update controller encode password

--- src/main/java/com/abelardo/isika/springbootsecurityjwt/controllers/AuthController.java
+++ src/main/java/com/abelardo/isika/springbootsecurityjwt/controllers/AuthController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -183,6 +184,21 @@ public class AuthController {
 	      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	    } catch (Exception e) {
 	      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	  }
+	
+	@PutMapping("/users/{id}")
+	 public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+	    Optional<User> userData = userRepository.findById(id);
+	    if (userData.isPresent()) {
+	      User _user = userData.get();
+	      _user.setUsername(user.getUsername());
+	      _user.setEmail(user.getEmail());
+	      _user.setPassword(encoder.encode(user.getPassword()));
+	      _user.setRoles(user.getRoles());
+	      return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
+	    } else {
+	      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	    }
 	  }
 
